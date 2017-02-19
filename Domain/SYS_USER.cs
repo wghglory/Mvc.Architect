c4cc4137625c97fe48de9b5ac7c6ff24f72a3faa
Guanghui Wang <wghglory89@gmail.com>
2017-02-18 17:26:36 -0800

    create EF models based on database

--- Domain/SYS_USER.cs
+++ Domain/SYS_USER.cs
@@ -14,6 +14,16 @@ namespace Domain
     
     public partial class SYS_USER
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SYS_USER()
+        {
+            this.SYS_POST_USER = new HashSet<SYS_POST_USER>();
+            this.SYS_USER_DEPARTMENT = new HashSet<SYS_USER_DEPARTMENT>();
+            this.SYS_USER_PERMISSION = new HashSet<SYS_USER_PERMISSION>();
+            this.SYS_USER_ROLE = new HashSet<SYS_USER_ROLE>();
+            this.SYS_USERINFO = new HashSet<SYS_USERINFO>();
+        }
+    
         public int ID { get; set; }
         public string NAME { get; set; }
         public string ACCOUNT { get; set; }
@@ -31,5 +41,16 @@ namespace Domain
         public string UPDATEUSER { get; set; }
         public Nullable<System.DateTime> UPDATEDATE { get; set; }
         public string LastLoginIP { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SYS_POST_USER> SYS_POST_USER { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SYS_USER_DEPARTMENT> SYS_USER_DEPARTMENT { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SYS_USER_PERMISSION> SYS_USER_PERMISSION { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SYS_USER_ROLE> SYS_USER_ROLE { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SYS_USERINFO> SYS_USERINFO { get; set; }
     }
 }
--- Domain/Sql_Wkdb.Context.cs
+++ Domain/Sql_Wkdb.Context.cs
@@ -26,5 +26,21 @@ namespace Domain
         }
     
         public virtual DbSet<SYS_USER> SYS_USER { get; set; }
+        public virtual DbSet<SYS_CODE> SYS_CODE { get; set; }
+        public virtual DbSet<SYS_CODE_AREA> SYS_CODE_AREA { get; set; }
+        public virtual DbSet<SYS_DEPARTMENT> SYS_DEPARTMENT { get; set; }
+        public virtual DbSet<SYS_LOG> SYS_LOG { get; set; }
+        public virtual DbSet<SYS_MODULE> SYS_MODULE { get; set; }
+        public virtual DbSet<SYS_PERMISSION> SYS_PERMISSION { get; set; }
+        public virtual DbSet<SYS_POST> SYS_POST { get; set; }
+        public virtual DbSet<SYS_POST_DEPARTMENT> SYS_POST_DEPARTMENT { get; set; }
+        public virtual DbSet<SYS_POST_USER> SYS_POST_USER { get; set; }
+        public virtual DbSet<SYS_ROLE> SYS_ROLE { get; set; }
+        public virtual DbSet<SYS_ROLE_PERMISSION> SYS_ROLE_PERMISSION { get; set; }
+        public virtual DbSet<SYS_SYSTEM> SYS_SYSTEM { get; set; }
+        public virtual DbSet<SYS_USER_DEPARTMENT> SYS_USER_DEPARTMENT { get; set; }
+        public virtual DbSet<SYS_USER_PERMISSION> SYS_USER_PERMISSION { get; set; }
+        public virtual DbSet<SYS_USER_ROLE> SYS_USER_ROLE { get; set; }
+        public virtual DbSet<SYS_USERINFO> SYS_USERINFO { get; set; }
     }
 }
